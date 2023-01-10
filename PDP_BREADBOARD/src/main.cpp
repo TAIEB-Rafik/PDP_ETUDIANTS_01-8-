7db658b6a81c0fee6f1fb9af39cbc9da6a21bb70
RafikTaieb <mtayeb.rafik@gmail.com>
2023-01-10 17:29:30 +0100

    temp hum

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+#define DHTTYPE DHT11  
+
+DHT dht(SENSOR, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println(F("DHT test!"));
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  // Effectuer une mesure toutes les 5s
+  delay(5000);
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   float f = dht.readTemperature(true);
+
+  // Afficher l'humidité relative avec un peu de formatage
+  Serial.print(F("Humidity: "));
+  Serial.println(h);
+
+  // Afficher la température avec un peu de formatage
+  Serial.print(F("%  Temperature: "));
+  Serial.println(t);
 }
