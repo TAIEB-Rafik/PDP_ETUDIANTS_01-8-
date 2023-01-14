2b09790085dc4df35197a9ad1d8f5c95423a7f4e
RafikTaieb <mtayeb.rafik@gmail.com>
2023-01-14 23:19:59 +0100

    etape3

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -3,12 +3,24 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTPIN 33 // Digital pin connected to the DHT sensor
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
 
-#define DHTTYPE DHT11
+#define DHTPIN 33 // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+// #define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE DHT11 // DHT 22 (AM2302)
+// #define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 // See guide for details on sensor wiring and usage:
 //   https://learn.adafruit.com/dht/overview
+
+#define Conversion 1000000 // Conversion de ms à secondes
+#define TIME_TO_SLEEP 5    // Temps de veille de l'ESP32 (en secondes)
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -17,80 +29,52 @@ uint32_t delayMS;
 void setup()
 {
   Serial.begin(9600);
-  // Initialize device.
+  // Initialise l'appareil
   dht.begin();
-  Serial.println(F("DHT11 Unified Sensor"));
-  // Print temperature sensor details.
+
+  pinMode(LED, OUTPUT);
+
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("%"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("%"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
-}
 
-void loop()
-{
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
+  // Afficher une température avec un peu de formatage
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature))
   {
-    Serial.println(F("<INFO> Error reading temperature!"));
+    Serial.println(F("Error reading temperature!"));
   }
   else
   {
-    Serial.print(F("<INFO> Temperature: "));
+    Serial.print(F("\n\nTemperature: "));
     Serial.print(event.temperature);
     Serial.println(F("°C"));
   }
-  // Get humidity event and print its value.
+
+  // Afficher l'humidité relative avec un peu de formatage
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity))
   {
-    Serial.println(F("<INFO> Error reading humidity!"));
+    Serial.println(F("Error reading humidity!"));
   }
   else
   {
-    Serial.print(F("<INFO> Humidity: "));
+    Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * Conversion);
+  Serial.println("<INFO> ESP32 Mise en veille  chaque " + String(TIME_TO_SLEEP) + " s");
+
+  Serial.println("<INFO> Mise en veille");
+  delay(1000);
+
+  Serial.flush();
+  esp_deep_sleep_start();
+  Serial.println("erreur ligne 75 executer");
+}
+
+void loop()
+{
 }
